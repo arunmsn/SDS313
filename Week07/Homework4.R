f5d17bf44d459dad0af2905c3c8a487a7ae63c2d
Arun <89404295+arunmsn@users.noreply.github.com>
2024-12-07 21:24:46 -0600

    Add files via upload

---
+++ Week07/Homework4.R
@@ -0,0 +1,16 @@
+bonds = read.csv("Week_7/Homework1_Bonds.csv")
+food = read.csv("Week_7/Homework4_food.csv")
+
+# Part 1
+""" 1. Create a subset that contains all of the bonds from Travis County.  
+Parse the election date variable and ensure that it was converted to a date variable correctly.  
+Investigate any dates that don’t make sense and either fix them or remove them from the dataset.  
+Briefly describe what you did in your report and provide the final number of bonds in your 
+Travis County subset (use this subset for #2 and #3 below).
+
+2. What month had the most bond election dates? Which day of the week had the most election dates? 
+
+3. Make a graph that compares the percentages of bonds that were carried across months and another 
+graph to compare the percentages that were carried across days of the week. Briefly summarize what 
+your graphs show in a few sentences.
+"""
